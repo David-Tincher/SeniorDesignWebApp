0f7385f5ecc00ccc4272f97e0f071e978cce2acc
David Tincher <tincher.15@wright.edu>
2025-10-21 08:39:35 -0400

    Please work

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,5 +1,14 @@
+from django.shortcuts import render
 from django.conf import settings
+import os
 
 def home(request):
-    image_url = settings.MEDIA_URL + 'images/randompic1.png'
-    return render(request, 'index.html', {'image_url': image_url})
+    images_dir = os.path.join(settings.STATICFILES_DIRS[0], 'images')
+    image_files = []
+
+    if os.path.exists(images_dir):
+        for filename in os.listdir(images_dir):
+            if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.webp')):
+                image_files.append(f'images/{filename}')
+
+    return render(request, 'index.html', {'image_files': image_files})
